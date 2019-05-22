f1237fcbd92319e53bb2eccf1ddbaa8deea541af
Oliver Schmitt <oliverschmitt@gmx.de>
2019-05-22 11:55:10 +0200

    Comments, set time to right value, store channel id uniquely

--- app/src/main/java/com/example/dbdelay/MainActivity.java
+++ app/src/main/java/com/example/dbdelay/MainActivity.java
@@ -14,14 +14,22 @@ import android.widget.Button;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
+    // Logging tag
     private static final String TAG = "MainActivity";
 
-    private static final int HOUR_OF_DAY = 5;
-    private static final String CHANNEL_ID = "NotificationChannel";
-
+    // Point in time
     private Calendar calendar;
+    private static final int HOUR_OF_DAY = 5;
+    private static final int MINUTE = 0;
+    private static final int SECOND = 0;
 
+    // Notification channel id
+    private static final String CHANNEL_ID = "NotificationChannel";
+    public static String getChannelId() {
+        return CHANNEL_ID;
+    }
+
+    // Alarm
     private AlarmManager alarmMgr;
     private PendingIntent alarmIntent;
 
@@ -30,18 +38,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Name", NotificationManager.IMPORTANCE_DEFAULT);
+        // Notification channel
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "DBDelay",
+                NotificationManager.IMPORTANCE_DEFAULT);
         NotificationManager notificationManager = getSystemService(NotificationManager.class);
         notificationManager.createNotificationChannel(channel);
 
-        // Specify time to make the requests
+        // Specify the time to perform the intent
         calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
         calendar.set(Calendar.HOUR_OF_DAY, HOUR_OF_DAY);
-        calendar.set(Calendar.MINUTE, 26);
-        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MINUTE, MINUTE);
+        calendar.set(Calendar.SECOND, SECOND);
 
-        // Specify the intent to perform
+        // Create the intent to perform
         Intent intent = new Intent(this, RequestHandler.class);
         alarmIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 
@@ -57,6 +67,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: Start");
+                // Cancel existing alarm
+                alarmMgr.cancel(alarmIntent);
+                // Set alarm and repeat once a day
                 alarmMgr.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
                         AlarmManager.INTERVAL_DAY, alarmIntent);
 
@@ -67,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: Stop");
+                // Cancel alarm
                 alarmMgr.cancel(alarmIntent);
             }
         });
--- app/src/main/java/com/example/dbdelay/RequestHandler.java
+++ app/src/main/java/com/example/dbdelay/RequestHandler.java
@@ -1,8 +1,5 @@
 package com.example.dbdelay;
 
-import android.app.Activity;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -12,19 +9,20 @@ import android.util.Log;
 
 public class RequestHandler extends BroadcastReceiver {
     private static final String TAG = "RequestHandler";
-    private static final String CHANNEL_ID = "NotificationChannel";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "onReceive: Intent received");
+        // Create the notification
+        String CHANNEL_ID = MainActivity.getChannelId();
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle("Title")
                 .setContentText("Content")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
+        // Send the notification
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-
         notificationManager.notify(0, builder.build());
 
     }
